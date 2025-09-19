efbc3af6e9764df037ee72721088711525b0ab66
Loqi <ProFoundNine856@gmail.com>
2025-09-19 15:47:14 +0300

    added window script + test text

--- Terminal.py
+++ Terminal.py
@@ -1,2 +1,12 @@
-import _tkinter as tk #GUI
+import tkinter as tk #GUI
 from tkinter import scrolledtext #полоска-слайдер
+
+window = tk.Tk() #создание окна
+window.geometry("1600x900")
+window.title("Terminal Emulator")
+
+label = tk.Label(window, text="Тестовое Окно")
+label.pack(pady=20)
+
+window.mainloop()
+
